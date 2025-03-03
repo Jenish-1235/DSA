a439d8bdf638ebd1316cbbfaf38b1bef8f12e323
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-03-04 04:17:51 +0530

    Sum of Digits using Recursion

---
+++ src/main/java/com/java/recursion/SumOfDigits.java
@@ -0,0 +1,21 @@
+package com.java.recursion;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("Sum of Digits: " + sumOfDigits(n));
+    }
+
+    public static int sumOfDigits(int n) {
+        if (n % 10 == 0){
+            return 0;
+        }
+
+        return n%10 + sumOfDigits(n/10);
+
+    }
+}
